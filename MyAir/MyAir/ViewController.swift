d84827159384918ebc57a3f63a3dd1b3502ac73b
Łukasz Miłaszewski <lukaszmilaszewski@ukaszs-MacBook-Pro.local>
2018-01-07 18:35:55 +0100

    added initial project with alamofire

---
+++ MyAir/MyAir/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyAir
+//
+//  Created by Łukasz Miłaszewski on 07/01/2018.
+//  Copyright © 2018 Łukasz Miłaszewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
